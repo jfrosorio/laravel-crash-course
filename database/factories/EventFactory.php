d294dd8a38047b37e98e7739c55ccc729bd1cb6a
José Osório <jfrosorio@gmail.com>
2021-02-26 11:01:41 +0000

    chore(lesson): exercises

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Event;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EventFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Event::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(5),
+            'description' => $this->faker->text(80),
+            'observations' => $this->faker->text(100),
+            'image_url' => $this->faker->imageUrl(),
+        ];
+    }
+}
