a19e858d941889d250054d14d4510d6efb53f9db
José Osório <jfrosorio@gmail.com>
2021-03-08 11:20:34 +0000

    chore(lesson): exercises

--- resources/lang/pt/auth.php
+++ resources/lang/pt/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'inactive' => 'Your account has been deactivated!',
+    'failed' => 'Credenciais inválidas',
+    'inactive' => 'Conta inativa!',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
